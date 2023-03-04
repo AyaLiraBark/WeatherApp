6c0413fdb7326b1024fb51e549fb05e8846f341c
AyaLiraBark <ayaboubnane2003@example.com>
2023-03-04 22:47:23 +0100

    font

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,54 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Weather App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="/myStyle/welcome.css">
+</head>
+
+<body>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col mt-6 cont1">
+                <div class="pad">
+                    <input type="text" class="cityInput">
+                    <h2 class="cityName">City Name</h2>
+                    <h5 class="dateThing">Sept 04, 2023</h5>
+                    <a href="#" class="weatherIcon"></a>
+                    <img src="imgs/storm.png" alt="weatherIcon" class="weatherIcon">
+                    <h2 class="weatherState">Cloudy</h2>
+                </div>
+
+            </div>
+            <div class="col mt-6 cont2">
+                <h1 class="degreeThing">57°</h1>
+                <h5 class="degreeShit">80°/16°</h5>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+
+
+    {{-- details by idk --}}
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
